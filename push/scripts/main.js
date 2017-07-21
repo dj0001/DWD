6cc866cae29637e709e330153061aefb612426ea
dj <dj0@gmx.de>
2017-07-21 14:07:34 +0200

    Update main.js

--- push/scripts/main.js
+++ push/scripts/main.js
@@ -173,3 +173,25 @@ function updateBtn() {
 
   pushButton.disabled = false;
 }
+
+
+
+//dj
+function showNotification() {
+  Notification.requestPermission(function(result) {
+    if (result === 'granted') {
+      navigator.serviceWorker.ready.then(function(registration) {
+        registration.showNotification('Vibration Sample', {
+          body: 'Buzz! Buzz!',
+          icon: '../images/touch/chrome-touch-icon-192x192.png',
+          vibrate: [200, 100, 200, 100, 200, 100, 200],
+          tag: 'vibration-sample'
+        });
+      });
+    }
+  });
+}
+
+
+
+setTimeout(function(){ showNotification(); }, 3000)
