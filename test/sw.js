c8f430ada348be99f2c6d9a0d2fe72a9015059eb
dj <dj0@gmx.de>
2017-07-23 17:25:06 +0200

    Update sw.js

--- test/sw.js
+++ test/sw.js
@@ -53,7 +53,7 @@ function showNotification(tx) {
     if (result === 'granted') {
       navigator.serviceWorker.ready.then(function(registration) {
         registration.showNotification('Vibration Sample', {
-          body: 'Buzz! '+tx  //Buzz!
+          body: 'Buzz! '+tx,  //Buzz!
           icon: '../images/touch/chrome-touch-icon-192x192.png',
           vibrate: [200, 100, 200, 100, 200, 100, 200],
           tag: 'vibration-sample'
