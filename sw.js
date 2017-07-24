be64705c9d26e40929583b36aaca4c3bc0865710
dj <dj0@gmx.de>
2017-07-24 18:53:21 +0200

    Update sw.js
    add notificationclick to fake serviceworker

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
 
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('http://www.dwd.de/DE/wetter/warnungen/warnWetter_node.html')  //https://developers.google.com/web/
+  );
+});
