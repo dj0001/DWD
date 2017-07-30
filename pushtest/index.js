94b60ac992d20b7250cdedde17d4c91f139caf04
dj <dj0@gmx.de>
2017-07-30 07:45:51 +0200

    Update index.js

--- pushtest/index.js
+++ pushtest/index.js
@@ -22,7 +22,7 @@ navigator.serviceWorker.register('sw_pa.js')  //service-worker.js
   document.getElementById('curl').textContent = 'curl -H "TTL: 60" -X POST ' + endpoint;
 
   // Send the subscription details to the server using the Fetch API.
-  fetch('https://android.googleapis.com/gcm/send', {  //  need endpoint?!!!./register
+  fetch('./register', {  //  need endpoint?!!!
     method: 'post',
     headers: {
       'Content-type': 'application/json'
