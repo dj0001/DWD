097d3fbddf86edb4d92595f07b982298a58134d6
dj <dj0@gmx.de>
2017-12-22 09:59:23 +0100

    add notificationclick
    open window

--- new/sw.js
+++ new/sw.js
@@ -1,1 +1,9 @@
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
 
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('/')
+  );
+});
