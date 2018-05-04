700b7a6e020aeee2047dbfc0a573f5e74f05018f
dj <dj0@gmx.de>
2018-05-04 08:43:47 +0200

    addon for DWD warnmodul

--- new/philipshue.js
+++ new/philipshue.js
@@ -2,18 +2,29 @@
 //lights go on for warnings
 //tested with emulator, because i don't have a bridge
 
-warnlayer._marker.on('move', function(e){ var data=warnlayer._data
+var path=opt.light  //?&light=1
+path=path? "lights/"+path+"/state" :"groups/0/action"
+
+var dt=0; warnlayer._marker.on('move', function(e){ var data=warnlayer._data
 if(data.features.length) {
+var severity=["Minor","Moderate","Severe","Extreme"], max=0  //get the highest warnlevel
+data.features.forEach(function(item){ item=item.properties
+max=Math.max(max,severity.indexOf(item.SEVERITY))
+})
 
+var color=[{"hue":10920,"bri":127,"sat":255,on:true},{"hue":5481,"bri":127,"sat":255,on:true},{"hue":0,"bri":127,"sat":255,on:true},{"hue":0,"bri":127,"sat":255,on:true}]
+var bd=color[max]  //{on:true}
+
+if(!isNaN(qs) && max >= qs-1+dt) {showLights(bd); dt++} else dt=0  //warnlev
+}
+})
+
+function showLights(bd) {
 var bridge="http://localhost:8000/api/newdeveloper"  //edit here
-var path="groups/0/action"  // lights/1/state
-var bd={on:true}  // {"hue":10920,"bri":127,"sat":255}
-
 var xhr = new XMLHttpRequest();
 xhr.open("PUT", bridge+"/"+path)
 xhr.setRequestHeader('Content-Type', 'application/json');
 xhr.onload = function() {console.log(xhr.responseText)}
+xhr.onerror = function(e) {console.log("error")}
 xhr.send(JSON.stringify(bd))
 }
-
-})
